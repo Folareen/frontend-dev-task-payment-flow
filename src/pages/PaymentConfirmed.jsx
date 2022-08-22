f77ab9b6dc4c37710b978c5339d329e20bc17a5f
Folareen <sakawahab03@gmail.com>
2022-08-23 00:38:09 +0100

    payment confirmed page

--- src/pages/PaymentConfirmed.jsx
+++ src/pages/PaymentConfirmed.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { FaCheck } from 'react-icons/fa'
 
-export const PaymentConfirmed = () => {
+const PaymentConfirmed = () => {
   return (
-    <div>PaymentConfirmed</div>
+    <div className='payment--confirmed'>
+      <div className='check'>
+        <FaCheck/>
+      </div>
+      <h1>
+        Purchase Completed
+      </h1>
+      <p>
+        Please check your email for details concerning 
+        this transaction
+      </p>
+      <Link to='/'>
+        Return Home
+      </Link>
+    </div>
   )
 }
+
+export default PaymentConfirmed
