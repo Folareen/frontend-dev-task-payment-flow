89864904df5be147bc5f2e236b275d9e79978b71
Folareen <sakawahab03@gmail.com>
2022-08-23 00:18:23 +0100

    confirm payment page

--- src/pages/ConfirmPayment.jsx
+++ src/pages/ConfirmPayment.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import Header from '../components/Header'
 
 const ConfirmPayment = () => {
   return (
-    <div>ConfirmPayment</div>
+    <section>
+      <div>
+        <Header/>
+      </div>
+      <div className='confirm--payment'>
+        <div className='confirm--payment--header'>
+          <h4>
+            Item Name
+          </h4>
+          <p>
+            #Price
+          </p>
+        </div>
+        <div className='confirm--payment--body'>
+          <div>
+            <p>Data science and usability</p>
+            <p>50,000.00</p>
+          </div>
+          <div>
+            <p>Shipping</p>
+            <p>0.00</p>
+          </div>
+        </div>
+        <div className='confirm--payment--footer'>
+          <div>
+            <p>Total</p>
+            <p>50,000.00</p>
+          </div>
+
+        </div>
+      </div>
+      <div className='submit'>
+        <button className='submit--button'>Next</button>
+        <button>Cancel Payment</button>
+      </div>
+    </section>
   )
 }
 
